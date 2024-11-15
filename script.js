a3583e4ca2d691c0607e4d06e57ecbe9e8b1fc71
Aleksandra Lysachok <aleksandra@lysachok.dev>
2024-11-15 12:39:17 -0500

    dogs data was added to description.

--- script.js
+++ script.js
@@ -16,22 +16,46 @@ const createGrid = async () => {
   const gridContainer = document.getElementById('grid-container');
 
   dogs.forEach(dog => {
-    console.log(dog)
-      const gridItem = document.createElement('div');
-      gridItem.classList.add('grid-item');
+      console.log(dog);
 
-      const img = document.createElement('img');
-      img.src = dog.url;
-      img.alt = `${dog.breed} Dog image `;
-      img.style.width = '100%';
-      img.style.borderRadius = '8px';
+      // Create the grid dinamicly and render only dogs that has breed name 
+      if (dog?.breeds?.[0]?.name) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
 
-      // const breedName = document.createElement('h2');
-      // breedName.textContent = dog.[0].id
+        // Create the image element
+        const img = document.createElement('img');
+        img.src = dog.url;
+        img.alt = `${dog?.breeds?.[0]?.name || "Unknown breed"} Dog image`;
+        img.style.width = '100%';
+        img.style.borderRadius = '8px';
 
-      gridItem.appendChild(img);
-      // gridItem.appendChild(breedName);
-      gridContainer.appendChild(gridItem);
+        const breedName = document.createElement('h2');
+        breedName.textContent = dog.breeds[0].name;
+
+        
+
+        const lifeSpan = document.createElement('p');
+        lifeSpan.textContent = `Life Span: ${dog.breeds[0].life_span}`;
+
+        const weight = document.createElement('p');
+        weight.textContent = `Weight (lb): ${dog.breeds[0].weight.imperial}`;
+
+        const temperament = document.createElement('p');
+        temperament.textContent = `Temperament: ${dog.breeds[0].temperament}`;
+      
+
+        // Append the image to the grid item
+        gridItem.appendChild(img);
+        gridItem.appendChild(breedName);
+        
+        gridItem.appendChild(lifeSpan);
+        gridItem.appendChild(weight);
+        gridItem.appendChild(temperament);
+        // Append the grid item to the grid container
+        gridContainer.appendChild(gridItem);
+        
+    }
   });
 };
 
